1c01775d9d32a74b46b63f8dcf375824803dec92
Rafsan <rafsan.quayes@gmail.com>
2022-06-12 14:54:07 +0600

    Done

---
+++ wp-content/plugins/woo-payment-bkash/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '2.1.0',
+        'version' => '2.1.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c02baa5b873e17fef0fc3a27f7023c8d603115de',
+        'name' => 'kapilpaul/payment-gateway-bkash-for-wc',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'reference' => '5c3fdc4945c8680bca6fb01eee1ec5dc4f22de87',
+            'dev_requirement' => false,
+        ),
+        'kapilpaul/payment-gateway-bkash-for-wc' => array(
+            'pretty_version' => '2.1.0',
+            'version' => '2.1.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c02baa5b873e17fef0fc3a27f7023c8d603115de',
+            'dev_requirement' => false,
+        ),
+    ),
+);
